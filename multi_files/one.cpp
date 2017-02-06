0bc10cd8bc78ce8f21afa9396221d18d3f4bb2be
fox769 <David.Yuan04@myhunter.cuny.edu>
2017-02-06 09:40:38 -0500

    Add files via upload

---
+++ multi_files/one.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+int main()
+{
+  int c;
+  std::cout << "HELLO WORLD\n";
+  
+  /*
+  c=add2(5,10);
+  std::cout << c << std::endl;
+  */
+
+  return 0;
+
+}
+
+int add2(int a, int b){
+
+  return a+b;
+
+}
